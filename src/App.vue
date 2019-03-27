f440196e676b36e6c1086617d3784df0dce93652
Dhruv Bhanushali <dhruv_b@live.com>
2019-03-27 16:36:06 +0530

    Add the <link> tag that links the OpenSearch description with the HTML

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,25 @@
     <router-view/>
   </div>
 </template>
+
 <script>
+import Vue from 'vue';
+import VueHead from 'vue-head';
+import openSearchDescription from '@/xml/open_search_description.xml';
+
+Vue.use(VueHead);
+
 export default {
   name: 'App',
+  head: {
+    link: [
+      {
+        rel: 'search',
+        type: 'application/opensearchdescription+xml',
+        href: openSearchDescription,
+      },
+    ],
+  },
 };
 </script>
 
