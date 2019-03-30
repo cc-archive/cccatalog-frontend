567b3af76c4535c28f68929cb6b80a179f6e57f5
theCelestial25 <spriyanshu25@gmail.com>
2019-03-30 10:09:54 +0530

    Fix component

--- test/unit/specs/components/search-grid-filter.spec.js
+++ test/unit/specs/components/search-grid-filter.spec.js
@@ -4,9 +4,7 @@ import render from '../../test-utils/render';
 describe('SearchGridFilter', () => {
 
   it('should render correct contents', () => {
-
     const wrapper = render(SearchGridFilter);
     expect(wrapper.find({ name: 'search-grid-filter' }).element).toBeDefined();
-    expect(wrapper.find('.search-filter_providers').element).toBeDefined();
   });
 });
