8c704b2258d9134a3f29f265e5aad1dbc427c886
Dhruv Bhanushali <dhruv_b@live.com>
2019-08-14 22:18:11 +0530

    Rebuild the Watermark component with complete i18n

--- src/components/Watermark.vue
+++ src/components/Watermark.vue
@@ -1,77 +1,115 @@
 <template>
-  <section class="sidebar_section">
-    <header class="sidebar_section-header">
-      <h2>
-        Image Download
-      </h2>
+  <section class="watermark">
+    <header>
+      <h3>{{ $t('downloadimage' )}}</h3>
     </header>
-    <div class="large-12 cell">
-      <fieldset class="large-7 cell">
-        <div>
-          <input
-            id="watermark"
-            type="checkbox"
-            v-model="shouldWatermark" />
-          <label for="watermark">
-            Include attribution frame
-          </label>
-          <help-tooltip :tooltip="watermarkHelp" />
-        </div>
-        <div>
-          <input id="embedAttribution"
-                  type="checkbox"
-                  v-model="shouldEmbedMetadata" />
-          <label for="embedAttribution">
-            Embed attribution metadata
-          </label>
-          <help-tooltip :tooltip="metadataHelp" />
-        </div>
-      </fieldset>
-      <button class="button download-watermark"
-              data-type="text"
-              @click="onDownloadWatermark(image, $event)">
-          Download Image
-      </button>
+    <div>
+      <ChoiceField v-model="customisations"
+                   id="watermark"
+                   color="blue"
+                   shade="dark"
+                   is-basic
+                   value="watermark"/>
+      <label for="watermark">
+        {{ $t('labels.include') }}
+      </label>
+      <help-tooltip :tooltip="$t('help.watermark')"/>
     </div>
+
+    <div>
+      <ChoiceField v-model="customisations"
+                   id="embedMetadata"
+                   color="blue"
+                   shade="dark"
+                   is-basic
+                   value="embedMetadata"/>
+      <label for="embedMetadata">
+        {{ $t('labels.embed') }}
+      </label>
+      <help-tooltip :tooltip="$t('help.metadata')"/>
+    </div>
+
+    <Button class="download"
+            color="blue"
+            shade="dark"
+            icon="download"
+            data-type="text"
+            @click="onDownloadWatermark(image, $event)">
+      {{ $t('download') }}
+    </Button>
   </section>
 </template>
 
 <script>
+import {
+  Button,
+  ChoiceField,
+} from '@creativecommons/vocabulary';
+
 import HelpTooltip from '@/components/HelpTooltip';
+
 import { DOWNLOAD_WATERMARK } from '@/store/action-types';
 
 export default {
   name: 'watermark',
-  props: ['image'],
   components: {
+    Button,
+    ChoiceField,
+
     HelpTooltip,
   },
+  props: {
+    image: {
+      type: Object,
+      required: true,
+    },
+  },
   data: () => ({
-    shouldEmbedMetadata: false,
-    shouldWatermark: false,
-    watermarkHelp: 'This option frames the image in white with a plain text attribution beneath.',
-    metadataHelp: 'This option embeds attribution and CC license metadata in the image file using XMP.',
+    customisations: [],
   }),
   computed: {
-    watermarkURL() {
-      return `${process.env.API_URL}/watermark/${this.image.id}?embed_metadata=${this.shouldEmbedMetadata}&watermark=${this.shouldWatermark}`;
+    shouldEmbedMetadata() {
+      return this.customisations.includes('embedMetadata');
+    },
+    shouldWatermark() {
+      return this.customisations.includes('watermark');
+    },
+    watermarkUrl() {
+      const queryParams = [];
+      queryParams.push(`embed_metadata=${this.shouldEmbedMetadata}`);
+      queryParams.push(`watermark=${this.shouldWatermark}`);
+
+      return `${process.env.API_URL}/watermark/${this.image.id}?${queryParams.join('&')}`;
     },
   },
   methods: {
     onDownloadWatermark(image) {
       const shouldEmbedMetadata = this.shouldEmbedMetadata;
       const shouldWatermark = this.shouldWatermark;
+
       this.$store.dispatch(DOWNLOAD_WATERMARK, {
         imageId: image.id,
         shouldWatermark,
         shouldEmbedMetadata,
       });
-      window.location = this.watermarkURL;
+
+      window.location = this.watermarkUrl;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-  @import '../styles/photodetails.scss';
+  @import "~@creativecommons/vocabulary/tokens";
+
+  .watermark {
+    .download {
+      min-width: 12em;
+
+      margin: $space-normal $space-zero;
+    }
+  }
 </style>
+
+<i18n src="../locales/components/Watermark.json">
+</i18n>
