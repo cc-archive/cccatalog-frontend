1f30196eb176122a17519102908ed4f3e98a1716
Dhruv Bhanushali <dhruv_b@live.com>
2019-08-14 22:56:46 +0530

    Refactor code and enable i18n in the component

--- src/components/HomeLicenseFilter.vue
+++ src/components/HomeLicenseFilter.vue
@@ -1,35 +1,53 @@
 <template>
   <div class="home-license-filter">
-    <span>I want something I can</span>
+    <span class="intro">{{ $t('want') }}:</span>
 
-    <div class="license-filters" v-for="(licenseType, index) in licenseTypes" :key="index">
-      <input :id="licenseType.code"
-              type="checkbox"
-              :checked="licenseType.checked"
-              @input="onFilterChanged(licenseType.code)" />
-      <label :for="licenseType.code">{{ licenseType.name }}</label>
+    <div v-for="(licenseType, index) in licenseTypes"
+         :key="index"
+         class="license-filters">
+      <ChoiceField v-model="licenses"
+                   :id="licenseType.code"
+                   :value="licenseType.code"
+                   color="blue"
+                   shade="dark"
+                   is-basic/>
+      <label :for="licenseType.code">
+        {{ $t(licenseType.code) }}
+      </label>
     </div>
   </div>
 </template>
 
 <script>
+import {
+  ChoiceField,
+} from '@creativecommons/vocabulary';
+
 import { SET_QUERY } from '@/store/mutation-types';
 
 export default {
   name: 'license-filter',
-  methods: {
-    onFilterChanged(code) {
-      this.licenseTypes.forEach((lt) => {
-        if (lt.code === code) {
-          // eslint-disable-next-line no-param-reassign
-          lt.checked = true;
-        }
-      });
-      const filter = this.licenseTypes
-        .filter(lt => lt.checked)
-        .map(filterItem => filterItem.code)
-        .join(',');
-
+  components: {
+    ChoiceField,
+  },
+  data() {
+    return {
+      licenseTypes: [
+        {
+          code: 'commercial',
+          checked: false,
+        },
+        {
+          code: 'modification',
+          checked: false,
+        },
+      ],
+      licenses: [],
+    };
+  },
+  watch: {
+    licenses(val) {
+      const filter = val.join(',');
       this.$store.commit(SET_QUERY, {
         query: {
           lt: filter,
@@ -37,26 +55,30 @@ export default {
       });
     },
   },
-  data() {
-    return {
-      licenseTypes: [
-        { code: 'commercial', name: 'Use for commercial purposes', checked: false },
-        { code: 'modification', name: 'Modify or adapt', checked: false },
-      ],
-    };
-  },
 };
 </script>
 
 <style lang="scss" scoped>
-.home-license-filter {
-  margin-top: 0.5em;
-  text-align: left;
-}
-span {
-  display: block;
-}
-.license-filters {
-  display: inline-block;
-}
+  @import "~@creativecommons/vocabulary/tokens";
+
+  .home-license-filter {
+    display: inline-block;
+
+    text-align: center;
+
+    margin-top: $space-normal;
+
+    .intro {
+      display: block;
+    }
+
+    .license-filters {
+      display: inline-block;
+
+      margin: $space-zero $space-normal;
+    }
+  }
 </style>
+
+<i18n src="../locales/components/HomeLicenseFilter.json">
+</i18n>
