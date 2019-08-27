47ea6412912f1a5b830c4e6b5951d8525b3556c7
Dhruv Bhanushali <dhruv_b@live.com>
2019-08-27 12:02:39 +0530

    Remove the i18n from the site title as per review by @brenoferreira

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -3,6 +3,14 @@
     <Header color="blue"
             shade="dark"
             :title="$t('search')">
+      <!-- Remove this template to revert to the i18n-compatible site title -->
+      <template #branding>
+        <img class="logo"
+             :alt="`CC ${$t('search')}`"
+             :title="`CC ${$t('search')}`"
+             src="../assets/cc-search-logo-white.png">
+      </template>
+
       <nav-section :showNavSearch="showNavSearch"/>
     </Header>
     <!-- @slot Content goes here -->
@@ -21,6 +29,7 @@ export default {
   name: 'header-section',
   components: {
     Header,
+
     NavSection,
   },
   props: {
@@ -31,6 +40,12 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  .logo {
+    height: 2rem;
+  }
+</style>
 
 <i18n src="../locales/components/HeaderSection.json">
 </i18n>
