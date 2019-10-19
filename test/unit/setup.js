be27a6e76ebfa10674de53a99847f9ac67c7ef0a
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2019-10-19 14:08:21 +0100

    Adds test setup file for mocking localStorage

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,3 +1,23 @@
 import Vue from 'vue';
 
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+});
+
 Vue.config.productionTip = false;
