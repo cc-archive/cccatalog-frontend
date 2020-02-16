9dce47ebda6d2281faab39147695fd8ffa6c3f15
nilshah98 <neel12@somaiya.edu>
2020-02-16 12:16:20 +0530

    Added CSS styles to fix issue-575

--- src/components/SearchGridFilter.vue
+++ src/components/SearchGridFilter.vue
@@ -117,7 +117,9 @@ export default {
 .search-filters {
   background: #fafafa;
   display: none;
-  height: 100%;
+  height: auto;
+  position: sticky;
+  top: 0px;
 
   label {
     color: #333333;
