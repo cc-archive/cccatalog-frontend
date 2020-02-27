80cefb248fc7d774ad2e9ac61d7ed069cf02d1ad
AyanChoudhary <darkrider251099@gmail.com>
2020-02-27 21:14:00 +0530

    fix:remove CommonChunksPlugin and refactor using slpitChunks

--- build/webpack-prod-conf.js
+++ build/webpack-prod-conf.js
@@ -76,36 +76,6 @@ const webpackConfig = merge(baseWebpackConfig, {
     new webpack.HashedModuleIdsPlugin(),
     // enable scope hoisting
     new webpack.optimize.ModuleConcatenationPlugin(),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks (module) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
-    }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      minChunks: Infinity
-    }),
-    // This instance extracts shared chunks from code splitted chunks and bundles them
-    // in a separate chunk, similar to the vendor chunk
-    // see: https://webpack.js.org/plugins/commons-chunk-plugin/#extra-async-commons-chunk
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'app',
-      async: 'vendor-async',
-      children: true,
-      minChunks: 3
-    }),
-
     // copy custom static assets
     new CopyWebpackPlugin([
       {
@@ -113,13 +83,38 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory + '/img',
       }
     ]),
-    // extract webpack runtime & manifest to avoid vendor chunk hash changing
-    // on every build.
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest'
-    }),
     new VueSSRClientPlugin()
-  ]
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        // split vendor js into its own file
+        vendor: {
+          name: 'vendor',
+          // any required modules inside node_modules are extracted to vendor
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'initial'
+        },
+        // extract webpack runtime and module manifest to its own file in order to
+        // prevent vendor hash from being updated whenever app bundle is updated
+        'manifest': {
+          name: 'manifest',
+          minChunks: Infinity
+        },
+        // This instance extracts shared chunks from code splitted chunks and bundles them
+        // in a separate chunk, similar to the vendor chunk
+        'app': {
+          name: 'vendor-async',
+          enforce: true,
+          minChunks: 3,
+          chunks: 'async'
+        },
+      },
+    },
+    runtimeChunk: true,
+    namedChunks: true,
+    namedModules: true,
+  }
 })
 
 if (config.build.productionGzip) {
