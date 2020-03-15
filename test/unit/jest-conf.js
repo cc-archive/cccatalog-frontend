4fdf1616ee8e9074c639864db362a8927f0ad285
satyaom <satyaom1998@gmail.com>
2020-03-15 12:06:50 +0530

    removed mapCoverage

--- test/unit/jest-conf.js
+++ test/unit/jest-conf.js
@@ -20,7 +20,6 @@ module.exports = {
   ],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
-  mapCoverage: false,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
