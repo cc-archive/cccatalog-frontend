64a4ae3fcb46d18af57d344bdb6c26312f97b6c5
satyaom <satyaom1998@gmail.com>
2020-03-15 13:20:03 +0530

    removed duplicate moduleNameMapper

--- test/unit/jest-conf.js
+++ test/unit/jest-conf.js
@@ -7,9 +7,7 @@ module.exports = {
     'json',
     'vue',
   ],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
+  
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
