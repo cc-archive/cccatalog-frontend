1ca51454e642c27e824c9451b21ac2e1c244bcb6
satyaom <satyaom1998@gmail.com>
2020-03-15 12:02:49 +0530

    mapCoverage change default value to false

--- test/unit/jest-conf.js
+++ test/unit/jest-conf.js
@@ -20,7 +20,7 @@ module.exports = {
   ],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
-  mapCoverage: true,
+  mapCoverage: false,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
