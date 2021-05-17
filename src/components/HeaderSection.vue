f9e626e75e563d35e7b64b975285399d01ad6b0a
Zack Krida <zack@creativecommons.org>
2021-05-17 19:10:57 -0400

    Update HeaderSection.vue

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,14 +1,6 @@
 <template>
   <header>
     <NavSection :key="$route.path" :show-nav-search="showNavSearch" />
-    <div
-      v-show="showDonate"
-      :class="{
-        'padding-bottom-bigger': needsPadding,
-      }"
-    >
-      <DonationBanner @onDismiss="hideDonate" />
-    </div>
     <slot />
   </header>
 </template>
@@ -16,34 +8,12 @@
 <script>
 import NavSection from '@/components/NavSection'
 import local from '@/utils/local'
-import DonationBanner from './DonationBanner'
 
 export default {
   name: 'HeaderSection',
   components: {
     NavSection,
-    DonationBanner,
   },
-  props: { showNavSearch: { type: Boolean, default: false } },
-  data() {
-    return {
-      showDonate: !local.get('hide-donation-banner'),
-    }
-  },
-  computed: {
-    // Only pad the donation banner when the current route requires it
-    needsPadding() {
-      return (
-        !this.$route.path.startsWith('/photos') &&
-        !(this.$route.path === '/search')
-      )
-    },
-  },
-  methods: {
-    hideDonate() {
-      local.set('hide-donation-banner', true)
-      this.showDonate = false
-    },
-  },
+  props: { showNavSearch: { type: Boolean, default: false } }
 }
 </script>
